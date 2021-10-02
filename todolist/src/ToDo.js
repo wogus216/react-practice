dacce87d556b4cd51a24651ddf5f9cf8521b8033
wogus216 <wogus21694@gmail.com>
2021-10-02 17:50:48 +0900

    todo 1차 실패

--- todolist/src/ToDo.js
+++ todolist/src/ToDo.js
@@ -1,24 +1,70 @@
 /* eslint-disable-next-line  */
 import React, { useState } from 'react';
 import ToDoList from './ToDoList';
+import styled from 'styled-components';
+
+const Circle = styled.button`
+  background: rgb(127, 203, 207);
+  z-index: 5;
+  cursor: pointer;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  border: none;
+  position: absolute;
+  left: 45%;
+  top: 63%;
+`;
 
 export default function ToDo() {
-  const [text, setText] = useState();
-  const change = (e) => {
-    setText(e.target.value);
-  };
-  let lists = [];
-
-  const onClick = (e) => {
-    lists.push(text);
-    console.log(lists);
-  };
+  const [Todo, setTodo] = useState(ToDoList);
+  const [input, setInput] = useState({
+    id: '',
+    content: '',
+  });
+  const { id, content } = input;
+  const [appear, setAppear] = useState(false);
   return (
-    <div className="content">
-      <h1 className="title">ToDoList 만들기</h1>
-      <input type="text" className="inputText" onChange={change} value={text} />
-      <button onClick={onClick}>추가</button>
-      <ToDoList />
+    <div className="wrap">
+      <h1>ToDoList</h1>
+      {Todo.map((con, i) => {
+        return (
+          <div>
+            <h3>{Todo[i].content}</h3>
+          </div>
+        );
+      })}
+      {input}
+      {appear === true ? (
+        <div>
+          <input
+            className="push"
+            onChange={(e) => {
+              setInput({
+                id: 4,
+                content: '되나',
+              });
+            }}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                let addTodo = [...Todo, input];
+                console.log(addTodo);
+                setTodo(addTodo);
+              }
+            }}
+            placeholder="할일을 입력 후 enter를 누르세요"
+          />
+        </div>
+      ) : null}
+      <Circle>
+        <div
+          onClick={() => {
+            setAppear(true);
+          }}
+        >
+          😊
+        </div>
+      </Circle>
     </div>
   );
 }
--- todolist/src/ToDoList.js
+++ todolist/src/ToDoList.js
@@ -1,19 +1,14 @@
-import React from 'react';
-
-export default function ToDoList() {
-  let arr = ['menu1', 'menu1', 'menu1', 'menu1', 'menu1'];
-
-  console.log(arr);
-  return (
-    <div className="listWrap">
-      <ul>
-        {arr.map((arr) => (
-          <div>
-            <li>{arr}</li>
-            <button>삭제</button>
-          </div>
-        ))}
-      </ul>
-    </div>
-  );
-}
+export default [
+  {
+    id: 0,
+    content: '코딩공부',
+  },
+  {
+    id: 1,
+    content: '밥 맛있게 먹기',
+  },
+  {
+    id: 2,
+    content: '꾸준히 하기',
+  },
+];
