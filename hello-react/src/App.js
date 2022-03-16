5a8b9868d30655a6959d2d02edcc57e26245ae35
wogus216 <wogus21694@gmail.com>
2022-03-17 06:58:41 +0900

    3장 완료

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import { Component } from "react";
 import MyComponent from "./components/MyComponent";
 import Counter from "./components/Counter";
+import Say from "./components/Say";
 class App extends Component {
   render() {
-    return <Counter />;
+    return <Say />;
   }
 }
 
