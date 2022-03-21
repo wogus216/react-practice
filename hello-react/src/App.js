28466d8cba547c30f540d061bdf15f867f2012bd
wogus216 <wogus21694@gmail.com>
2022-03-22 07:10:11 +0900

    6장 완료

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
 import { Component } from "react";
 
-import ValidationSample from "./components/ValidationSample";
-import ScrollBox from "./components/ScrollBox";
+import IterationSample from "./components/IterationSample";
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
