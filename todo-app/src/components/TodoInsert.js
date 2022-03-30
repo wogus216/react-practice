80d6b4f49a609042d274e452055fa8c6cdc99a2a
wogus216 <wogus21694@gmail.com>
2022-03-31 07:10:08 +0900

    10장 중간 완성

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,14 @@
+import './TodoInsert.scss';
+import { MdAdd } from 'react-icons/md';
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
