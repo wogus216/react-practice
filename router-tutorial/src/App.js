0319167379124666e7a470a336e641c568568b63
wogus216 <wogus21694@gmail.com>
2022-04-07 07:31:56 +0900

    라우터 부가 기능 전까지 완료

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import { Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Profiles from "./pages/Profiles";
 
 const App = () => {
   return (
@@ -13,9 +14,13 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
