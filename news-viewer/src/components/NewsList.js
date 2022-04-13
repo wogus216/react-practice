089fc05cae183235caf1faf431a7e28e4c4d6d85
wogus216 <wogus21694@gmail.com>
2022-04-14 07:04:14 +0900

    news api 연동 성공

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
 
@@ -14,23 +16,42 @@ margin-top: 2rem;
 }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "https://google.com",
-  urlToImage: "https://via.placeholder.com/160",
-};
-
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=kr&apiKey=bd7aacb8c47e4d0ca52a172a34697cc2"
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  //대기 중일 때
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  //아직 articles 값이 설정되지 않았을 때
+  if (!articles) {
+    return null;
+  }
+
+  //articles 값이 유효할 때
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
-      <NewsItem article={sampleArticle}></NewsItem>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
