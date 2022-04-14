7f3e56e3c26360ca32eb36028a9fb5ae6356d762
wogus216 <wogus21694@gmail.com>
2022-04-15 07:56:19 +0900

    동적 context 완료

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ColorBox from "./contexts/ColorBox";
+import  { ColorProvider } from "./contexts/color";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorProvider>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorProvider>
   );
 }
 
