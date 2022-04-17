f08265e6661d327fb3a333913078ea6212d1e37a
wogus216 <wogus21694@gmail.com>
2022-04-18 07:18:13 +0900

    useContext learned

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ColorBox from "./contexts/ColorBox";
-import  { ColorProvider } from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
+import SelectColor from "./components/SelectColors";
 
 function App() {
   return (
     <ColorProvider>
       <div>
+        <SelectColor />
         <ColorBox />
       </div>
     </ColorProvider>
