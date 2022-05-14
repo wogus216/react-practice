ede93eb25d5013f570928ffb280a8ccedbc6fe08
wogus216 <wogus21694@gmail.com>
2022-05-14 13:12:14 +0900

    17장 완료

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,13 +1,20 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-  );
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  // return <Counter number={number} onIncrease={() => dispatch(increase())} onDecrease={() => dispatch(decrease())} />;
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+  return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 };
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+// };
 
 //처음 방법
 // const mapStateProps = (state) => ({
@@ -23,6 +30,7 @@ const CounterContainer = ({ number, increase, decrease }) => {
 //     dispatch(decrease());
 //   },
 // });
+
 // 두번 째 방법
 // export default connect(
 //   (state) => ({
@@ -38,12 +46,13 @@ const CounterContainer = ({ number, increase, decrease }) => {
 //     )
 // )(CounterContainer);
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   }
+// )(CounterContainer);
+export default CounterContainer;
