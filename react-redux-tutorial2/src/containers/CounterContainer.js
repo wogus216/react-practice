cfa5452f31d76f1361c55681af73d74b080f7659
wogus216 <wogus21694@gmail.com>
2022-05-15 16:48:43 +0900

    redux-actions 부터

---
+++ react-redux-tutorial2/src/containers/CounterContainer.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Counter from '../components/Counter';
+import { decrease, increase } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  {
+    increase,
+    decrease,
+  }
+)(CounterContainer);
