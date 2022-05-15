8698b1af8029fbcb66d625217d77ea1c4d4ffb76
wogus216 <wogus21694@gmail.com>
2022-05-16 06:30:53 +0900

    미들웨어 시작

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CounterContainer from './containers/CounterContainer';
+
+function App() {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ learn-redux-middleware/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, legacy_createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import loggerMiddleware from './lib /loggerMiddleware';
+
+const store = legacy_createStore(rootReducer, applyMiddleware(loggerMiddleware));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
