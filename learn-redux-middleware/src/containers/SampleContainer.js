7574ea56ac74b187bfb3c1712e6bdd6784995239
wogus216 <wogus21694@gmail.com>
2022-05-25 06:06:06 +0900

    redux-saga 시작

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import Sample from '../components/Sample';
 import { getPost, getUsers } from '../modules/sample';
 
 const SampleContainer = ({ getPost, getUsers, post, users, loadingPost, loadingUsers }) => {
   // 클래스 형태의 컴포넌트 였다면 componentDidMount
+  //useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+  //그 내부에서 async 함수를 선언하고 호출해 줍니다.
   useEffect(() => {
     const fn = async () => {
       try {
