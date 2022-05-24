671aba9a0f9df38101fe15d4ae91d32e111382ae
wogus216 <wogus21694@gmail.com>
2022-05-25 07:50:30 +0900

    19장 완료

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,15 +1,20 @@
+import React, { useState, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const SplitMe = React.lazy(() => import('./SplitMe'));
 
 function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
-    import('./notify').then((result) => result.default());
+    setVisible(true);
   };
   return (
-    <div className="App">
+    <div className="APP">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!!</p>
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>} />
+        {visible && <SplitMe />}
       </header>
     </div>
   );
