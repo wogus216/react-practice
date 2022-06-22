d3f9b1f7c82e601c74a9af862fdcce8469385402
wogus216 <wogus21694@gmail.com>
2022-06-22 15:14:21 +0900

    redux 조금 씩 이해간다

--- 07-starting-project/src/components/Counter.js
+++ 07-starting-project/src/components/Counter.js
@@ -1,15 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const incrementHandler = () => {
+    dispatch({ type: 'increament' });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increase</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>decrease</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
+
+// class Counter extends Component{
+//   render(){
+//     return(
+
+//     )
+//   }
+// }
 
 export default Counter;
