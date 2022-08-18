3b949de5fa015e7957e74a82fd4758f1a966a054
wogus216 <wogus21694@gmail.com>
2022-08-18 23:07:11 +0900

    섹션 11 시작

---
+++ 11-starting-setup/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Button from '../src/components/UI/Button/Button';
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>toggleParagraphHandler!</Button>
+    </div>
+  );
+}
+
+export default App;
